b008dbdb37758c57e780d2743001c31c2a4989ea
Bartosz Korszun <bartoszkorszun@gmail.com>
2024-12-05 20:05:20 +0100

    Solved second part

--- day05/day05.kt
+++ day05/day05.kt
@@ -17,24 +17,27 @@ fun main() {
         line.split(",").map { it.toInt() }
     }
 
-    var isInOrder = true
     var count = 0
+    var repairedCount = 0
 
     for (update in updates) {
-        isInOrder = true
+        var isInOrder = true
         for (i in update.indices) {
             if (!checkRules(rules, update, i)) {
                 isInOrder = false
+                val repairedUpdate = repairUpdate(rules, update)
+                repairedCount += repairedUpdate[repairedUpdate.size / 2]
+                println("update: $update, repairedUpdate: $repairedUpdate")
                 break
             }
         }
 
         if (isInOrder) {
-            count += update[(update.size - 1)/2]
-        }
+            count += update[(update.size) / 2]
+        } 
     }
 
-    println(count)
+    println("count: $count, repairedCount: $repairedCount")
 }
 
 fun checkRules(rules: List<Pair<Int, Int>>, update: List<Int>, index: Int): Boolean {
@@ -48,4 +51,35 @@ fun checkRules(rules: List<Pair<Int, Int>>, update: List<Int>, index: Int): Bool
         }
     }
     return true
+}
+
+fun repairUpdate(rules: List<Pair<Int, Int>>, update: List<Int>): List<Int> {
+    val repairedUpdate = update.toMutableList()
+    while (true) {
+        var isRepaired = true
+        for (i in repairedUpdate.indices) {
+            if (!checkRules(rules, repairedUpdate, i)) {
+                isRepaired = false
+                swap(rules, repairedUpdate, i)
+                break
+            }
+        }
+        if (isRepaired) {
+            break
+        }
+    }
+    return repairedUpdate
+}
+
+fun swap(rules: List<Pair<Int, Int>>, repairedUpdate: MutableList<Int>, index: Int) {
+    for (rule in rules) {
+        if (repairedUpdate[index] == rule.first) {
+            for (i in index downTo 0) {
+                if (repairedUpdate[i] == rule.second && i < index) {
+                    repairedUpdate[index] = rule.second
+                    repairedUpdate[i] = rule.first
+                }
+            }
+        }
+    }
 }
