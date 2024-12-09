f3246a5b728233ddc8b6e017101edca3c868a623
Bartosz Korszun <bartoszkorszun@gmail.com>
2024-12-09 18:12:50 +0100

    solved part 1

--- day09/day09.kt
+++ day09/day09.kt
@@ -1,14 +1,14 @@
 import java.io.File
 
 fun main() {
-    val input = File("test.txt").readText().trim().map { it.toString().toInt() }.toIntArray()
+    val input = File("input.txt").readText().trim().map { it.toString().toInt() }.toIntArray()
     
-    val fileBlocks = Array((input.size - 1) / 2) { IntArray(3) }
+    val fileBlocks = Array((input.size + 1) / 2) { IntArray(3) }
     fillFileBlocks(input, fileBlocks)
 
-    for (i in fileBlocks.indices) {
-        println(fileBlocks[i].joinToString(", "))
-    }
+    var finalArr = createFinalArr(fileBlocks)
+
+    println(calculateScore(finalArr))
 }
 
 fun fillFileBlocks(input: IntArray, fileBlocks: Array<IntArray>) {
@@ -20,6 +20,39 @@ fun fillFileBlocks(input: IntArray, fileBlocks: Array<IntArray>) {
     fileBlocks[(input.size - 1) / 2][0] = (input.size - 1) / 2
     fileBlocks[(input.size - 1) / 2][1] = input[input.size - 1]
     fileBlocks[(input.size - 1) / 2][2] = 0
+}
+
+fun createFinalArr(fileBlocks: Array<IntArray>): MutableList<Int> {
+    val finalArr = mutableListOf<Int>()
+    var k = 0
+
+    for (i in fileBlocks.indices) {
+        while (fileBlocks[i][1] != 0) {
+            finalArr.add(fileBlocks[i][0])
+            fileBlocks[i][1]--
+        }
+        while (fileBlocks[i][2] != 0 && fileBlocks[fileBlocks.size - 1 - k][1] != 0) {
+            finalArr.add(fileBlocks[fileBlocks.size - 1 - k][0])
+            fileBlocks[i][2]--
+            fileBlocks[fileBlocks.size - 1 - k][1]--
+
+            if (fileBlocks[fileBlocks.size - 1 - k][1] == 0) {
+                k++
+            }
+        }
+    }
+
+    return finalArr
+}
+
+fun calculateScore(finalArr: MutableList<Int>): Long {
+    var score = 0L
+
+    for (i in finalArr.indices) {
+        score += finalArr[i] * i
+    }
+
+    return score
 }
 
 // 799772458
