65cf99da2cc7ed3b9ef0211f0897b7e54c57ff7d
Bartosz Korszun <bartoszkorszun@gmail.com>
2024-12-09 23:40:49 +0100

    solved second part

--- day09/day09.kt
+++ day09/day09.kt
@@ -3,56 +3,65 @@ import java.io.File
 fun main() {
     val input = File("input.txt").readText().trim().map { it.toString().toInt() }.toIntArray()
     
-    val fileBlocks = Array((input.size + 1) / 2) { IntArray(3) }
-    fillFileBlocks(input, fileBlocks)
+    val fileBlocks = fillFileBlocks(input)
 
-    var finalArr = createFinalArr(fileBlocks)
+    changeArr(fileBlocks)
 
-    println(calculateScore(finalArr))
+    println(calculateScore(fileBlocks))
 }
 
-fun fillFileBlocks(input: IntArray, fileBlocks: Array<IntArray>) {
+fun fillFileBlocks(input: IntArray): MutableList<IntArray> {
+    val fileBlocks = mutableListOf<IntArray>()
+
     for (i in 0 until input.size - 1 step 2) {
-        fileBlocks[i / 2][0] = i / 2
-        fileBlocks[i / 2][1] = input[i]
-        fileBlocks[i / 2][2] = input[i + 1]
+        val arr = intArrayOf(i / 2, input[i], input[i + 1])
+        fileBlocks.add(arr)
     }
-    fileBlocks[(input.size - 1) / 2][0] = (input.size - 1) / 2
-    fileBlocks[(input.size - 1) / 2][1] = input[input.size - 1]
-    fileBlocks[(input.size - 1) / 2][2] = 0
+
+    val arr = intArrayOf((input.size - 1) / 2, input[input.size - 1], 0)
+    fileBlocks.add(arr)
+
+    return fileBlocks
 }
 
-fun createFinalArr(fileBlocks: Array<IntArray>): MutableList<Int> {
-    val finalArr = mutableListOf<Int>()
-    var k = 0
-
-    for (i in fileBlocks.indices) {
-        while (fileBlocks[i][1] != 0) {
-            finalArr.add(fileBlocks[i][0])
-            fileBlocks[i][1]--
-        }
-        while (fileBlocks[i][2] != 0 && fileBlocks[fileBlocks.size - 1 - k][1] != 0) {
-            finalArr.add(fileBlocks[fileBlocks.size - 1 - k][0])
-            fileBlocks[i][2]--
-            fileBlocks[fileBlocks.size - 1 - k][1]--
-
-            if (fileBlocks[fileBlocks.size - 1 - k][1] == 0) {
-                k++
+fun changeArr(fileBlocks: MutableList<IntArray>) {
+    var i = fileBlocks.size - 1
+    var changed = false
+    var noPairs = false
+    while (i > 0) {
+        for (j in 0 until i + 1) {
+            if (fileBlocks[i][1] <= fileBlocks[j][2] && fileBlocks[i] != fileBlocks[j] && fileBlocks[i][0] != 0) {
+                fileBlocks[i - 1][2] += fileBlocks[i][1] + fileBlocks[i][2]
+                fileBlocks[i][2] = fileBlocks[j][2] - fileBlocks[i][1]
+                fileBlocks[j][2] = 0
+                val elem = fileBlocks.removeAt(i)
+                fileBlocks.add(j + 1, elem)
+                changed = true
+                break
+            }
+            if (j == i) {
+                noPairs = true
             }
         }
+        if (!changed || noPairs) {
+            i--
+            changed = false
+            noPairs = false
+        }
     }
-
-    return finalArr
 }
 
-fun calculateScore(finalArr: MutableList<Int>): Long {
+fun calculateScore(fileBlocks: MutableList<IntArray>): Long {
     var score = 0L
-
-    for (i in finalArr.indices) {
-        score += finalArr[i] * i
+    var multiplier = 0L
+    for (i in fileBlocks.indices) {
+        for (j in 0 until fileBlocks[i][1]) {
+            score += fileBlocks[i][0] * multiplier
+            multiplier++
+        }
+        for (j in 0 until fileBlocks[i][2]) {
+            multiplier++
+        }
     }
-
     return score
 }
-
-// 799772458
