a96ff8b118feace81ea9d9517af58c1b6a833d0e
Bartosz Korszun <bartoszkorszun@gmail.com>
2024-12-10 19:46:54 +0100

    solved second part

--- day10/day10.kt
+++ day10/day10.kt
@@ -9,9 +9,7 @@ fun main() {
     for (i in arr.indices) {
         for (j in arr[i].indices) {
             if (arr[i][j] == 0) {
-                val positions = mutableListOf<Pair<Int, Int>>()
-                findPath(arr, i, j, positions)     
-                sum += positions.distinct().count()
+                sum += findPath(arr, i, j)     
             }
         }
     }
@@ -23,27 +21,29 @@ fun createArr(input: List<String>): Array<IntArray> {
     return Array(input.size) { i -> input[i].map { it.toString().toInt() }.toIntArray() }
 }
 
-fun findPath(arr: Array<IntArray>, i: Int, j: Int, positions: MutableList<Pair<Int, Int>>) {
+fun findPath(arr: Array<IntArray>, i: Int, j: Int): Int {
     if (arr[i][j] == 9) {
-        positions.add(Pair(i, j))
-        return
+        return 1
     }
 
     val up = i - 1
     val down = i + 1
     val left = j - 1
     val right = j + 1
+    var count = 0
 
     if (up >= 0 && arr[up][j] - arr[i][j] == 1) {
-        findPath(arr, up, j, positions)
+        count += findPath(arr, up, j)
     }
     if (down < arr.size && arr[down][j] - arr[i][j] == 1) {
-        findPath(arr, down, j, positions)
+        count += findPath(arr, down, j)
     }
     if (left >= 0 && arr[i][left] - arr[i][j] == 1) {
-        findPath(arr, i, left, positions)
+        count += findPath(arr, i, left)
     }
     if (right < arr[i].size && arr[i][right] - arr[i][j] == 1) {
-        findPath(arr, i, right, positions)
+        count += findPath(arr, i, right)
     }
+
+    return count
 }
