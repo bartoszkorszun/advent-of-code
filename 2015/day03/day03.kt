d5f1af975a51d89af9906c5a0e3251b7b762b9b1
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2024-12-19 09:51:01 +0100

    solved part 2

--- 2015/day03/day03.kt
+++ 2015/day03/day03.kt
@@ -1,22 +1,6 @@
 import java.io.File
 
-fun main() {
-    val input = File("input.txt").readText().trim()
-    input.toCharArray()
-
-    val locations = mutableSetOf<Pair<Int, Int>>()
-    var position = Pair(0, 0)
-    locations.add(position)
-    for (c in input) {
-        position = move(c, position)
-        locations.add(position)
-    }
-
-    println(locations.size)
-}
-
 fun move(c: Char, position: Pair<Int, Int>): Pair<Int, Int> {
-
     var (x, y) = position
 
     when (c) {
@@ -27,4 +11,44 @@ fun move(c: Char, position: Pair<Int, Int>): Pair<Int, Int> {
     }
 
     return Pair(x, y)
+}
+
+fun part1(input: String): Int {
+    val locations = mutableSetOf<Pair<Int, Int>>()
+    var position = Pair(0, 0)
+    locations.add(position)
+    for (c in input) {
+        position = move(c, position)
+        locations.add(position)
+    }
+
+    return locations.size
+}
+
+fun part2(input: String): Int {
+    val locations = mutableSetOf<Pair<Int, Int>>()
+    var santasPosition = Pair(0, 0)
+    var robotsPosition = Pair(0, 0)
+    locations.add(santasPosition)
+    for (i in input.indices) {
+        if (i % 2 == 0) {
+            santasPosition = move(input[i], santasPosition)
+            locations.add(santasPosition)
+        } else {
+            robotsPosition = move(input[i], robotsPosition)
+            locations.add(robotsPosition)
+        }
+    }
+
+    return locations.size
+}
+
+fun main() {
+    val input = File("input.txt").readText().trim()
+
+    val p1 = part1(input)
+    val p2 = part2(input)
+
+    println("Part 1: $p1")
+    println("Part 2: $p2")
 }
