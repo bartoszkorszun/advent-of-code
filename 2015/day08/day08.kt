2265024ed83eb7922e2fe07f97f239d6d0d7886f
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-02-05 21:42:53 +0100

    solved part 1

---
+++ 2015/day08/day08.kt
@@ -0,0 +1,30 @@
+import java.io.File
+
+fun part2(input: String): Int {
+    return 0
+}
+
+fun part1(input: String): Int {
+    val lines = input.lines()
+    var totalCode = 0
+    var totalMemory = 0
+    lines.forEach {
+        totalCode += it.length
+        totalMemory += it.substring(1, it.length - 1)
+        .replace("\\\\", "a")
+        .replace("\\\"", "a")
+        .replace(Regex("""\\x[a-f0-9]{2}"""), "a")
+        .length
+    }
+    return totalCode - totalMemory
+}
+
+fun main() {
+    val input = File("input.txt").readText().trim()
+    
+    val p1 = part1(input)
+    val p2 = part2(input)
+
+    println("Part 1: $p1")   
+    println("Part 2: $p2")
+}
