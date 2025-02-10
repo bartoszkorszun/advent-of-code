ba25c1b76cda1f33828e72b9add1d115666bfd0b
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-02-10 12:08:45 +0100

    solved part 2

--- 2015/day10/day10.kt
+++ 2015/day10/day10.kt
@@ -1,18 +1,38 @@
 import java.io.File
 
-fun part2(input: String): Int {
-    return 0
+fun calculateResult(input: String, iterations: Int): Int {
+    var result = StringBuilder(input)
+    repeat(iterations) {
+        val newInput = StringBuilder()
+        var i = 0
+        while (i < result.length) {
+            var count = 1
+            while (i + 1 < result.length && result[i] == result[i + 1]) {
+                count++
+                i++
+            }
+            newInput.append(count)
+            newInput.append(result[i])
+            i++
+        }
+        result = newInput
+    }
+    return result.length
 }
 
+fun part2(input: String): Int {
+    return calculateResult(input, 50)
+}   
+
 fun part1(input: String): Int {
-    return 0
+    return calculateResult(input, 40)
 }
 
 fun main() {
     val input = File("input.txt").readText().trim()
     
-    val p1 = part1(locations)
-    val p2 = part2(locations)
+    val p1 = part1(input)
+    val p2 = part2(input)
 
     println("Part 1: $p1")   
     println("Part 2: $p2")
