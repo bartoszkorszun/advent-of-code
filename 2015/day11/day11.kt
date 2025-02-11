c9f48c5f7202ef6249395e77050cd68d4deecaf2
bartoszkorszun <bartoszkorszun@gmail.com>
2025-02-11 07:23:04 +0100

    solved part 2

--- 2015/day11/day11.kt
+++ 2015/day11/day11.kt
@@ -1,7 +1,7 @@
 import java.io.File
 
-fun part2(input: String): Int {
-    return 0
+fun part2(input: String): String {
+    return part1(input)
 }   
 
 fun part1(input: String): String {
@@ -19,7 +19,7 @@ fun main() {
     val input = File("input.txt").readText().trim()
     
     val p1 = part1(input)
-    val p2 = part2(input)
+    val p2 = part2(p1)
 
     println("Part 1: $p1")   
     println("Part 2: $p2")
