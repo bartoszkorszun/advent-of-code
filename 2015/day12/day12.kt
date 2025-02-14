09d0cead7307bfa53af09639d1d7c5d3de648ebc
Bartosz Korszun <61890250+bartoszkorszun@users.noreply.github.com>
2025-02-14 15:33:28 +0100

    Update day12.kt

--- 2015/day12/day12.kt
+++ 2015/day12/day12.kt
@@ -1,12 +1,40 @@
 import java.io.File
+import java.util.*
+
+fun removeRedObjects(input: String): String {
+    val sb = StringBuilder(input)
+    val stack = Stack<Int>()
+    
+    var i = 0
+    while (i < sb.length) {
+        if (sb[i] == '{') {
+            stack.push(i) 
+        } else if (sb[i] == '}') {
+            val start = stack.pop()
+            val obj = sb.substring(start, i + 1)
+            if (Regex(""":\s*"red"+""").containsMatchIn(obj)) {
+                sb.replace(start, i + 1, " ".repeat(i + 1 - start))
+                i = start
+            }
+        }
+        i++
+    }
+
+    return sb.toString()
+}
+
+fun sumNumbers(input: String): Int {
+    val regex = Regex("-?\\d+")
+    return regex.findAll(input).sumOf { it.value.toInt() }
+}
 
 fun part2(input: String): Int {
-    return 0    
+    val modifiedInput = removeRedObjects(input)
+    return sumNumbers(modifiedInput)
 }
 
 fun part1(input: String): Int {
-    val regex = Regex("-?\\d+")
-    return regex.findAll(input).sumOf { it.value.toInt() }
+    return sumNumbers(input)
 }
 
 fun main() {
