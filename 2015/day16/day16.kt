5fed3bb311ee5fa70610ad237b3d163c6463a25b
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-02-26 20:39:44 +0100

    initial commit day 16 and solved part 1

---
+++ 2015/day16/day16.kt
@@ -0,0 +1,64 @@
+import java.io.File
+
+data class Aunt(
+    val number: Int, 
+    val properties: Map<String, Int>
+)
+
+data class Criteria(
+    val properties: Map<String, Int>
+)
+
+fun parseInput(input: String, sCriteria: String): Pair<List<Aunt>, Criteria> {
+    val auntRegex = Regex("""Sue (\d+): (.+)""")
+    val criteriaRegex = Regex("""(\w+): (\d+)""")
+
+    val aunts = input.lines().map { line ->
+        val match = auntRegex.matchEntire(line)!!
+        val number = match.groupValues[1].toInt()
+        val properties = match.groupValues[2].split(", ").map { property ->
+            val (name, value) = property.split(": ")
+            name to value.toInt()
+        }.toMap()
+        Aunt(number, properties)
+    }
+
+    val properties = sCriteria.lines().map { line ->
+        val match = criteriaRegex.matchEntire(line)!!
+        val name = match.groupValues[1]
+        val value = match.groupValues[2].toInt()
+        name to value
+    }.toMap()
+
+    val criteria = Criteria(properties)
+
+    return Pair(aunts, criteria)
+}
+
+fun part2(input: List<Aunt>, criteria: Criteria): Int {
+    return 0
+}   
+
+fun part1(aunts: List<Aunt>, criteria: Criteria): Int {
+    aunts.forEach { aunt ->
+        val properties = aunt.properties
+        val match = properties.all { (name, value) ->
+            criteria.properties[name]?.let { it == value } ?: true
+        }
+        if (match) return aunt.number
+    }
+    return -1
+}
+
+fun main() {
+    val (aunts, criteria) = parseInput(
+        File("input.txt").readText().trim(),
+        File("criteria.txt").readText().trim()
+    )
+    
+    val p1 = part1(aunts, criteria)
+    val p2 = part2(aunts, criteria)
+
+    println("Part 1: $p1")   
+    println("Part 2: $p2")
+}
