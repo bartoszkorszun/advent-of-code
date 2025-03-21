145608fd8da3f1474613fe0a760ac92aeb76f914
bartoszkorszun <bartoszkorszun@gmail.com>
2025-03-21 20:02:26 +0100

    solved part 2

--- 2016/day02/day02.kt
+++ 2016/day02/day02.kt
@@ -1,10 +1,37 @@
 import java.io.File
 
-fun part2(input: String, keypad: Array<Array<Int>>): Int {
-    return 0
+fun part2(input: String): String {
+    val keypad = arrayOf(
+        arrayOf('0', '0', '1', '0', '0'),
+        arrayOf('0', '2', '3', '4', '0'),
+        arrayOf('5', '6', '7', '8', '9'),
+        arrayOf('0', 'A', 'B', 'C', '0'),
+        arrayOf('0', '0', 'D', '0', '0')
+    )
+    val lines = input.lines().map { it.toCharArray() }
+    var code: StringBuilder = StringBuilder()
+    var x = 0
+    var y = 2
+    for (line in lines) {
+        for (c in line) {
+            when (c) {
+                'U' -> if (y > 0 && keypad[y - 1][x] != '0') y--
+                'D' -> if (y < 4 && keypad[y + 1][x] != '0') y++
+                'L' -> if (x > 0 && keypad[y][x - 1] != '0') x--
+                'R' -> if (x < 4 && keypad[y][x + 1] != '0') x++
+            }
+        }
+        code.append(keypad[y][x].toString())
+    }
+    return code.toString()
 }   
 
-fun part1(input: String, keypad: Array<Array<Int>>): Int {
+fun part1(input: String): Int {
+    val keypad = arrayOf(
+        arrayOf(1, 2, 3),
+        arrayOf(4, 5, 6),
+        arrayOf(7, 8, 9)
+    )
     val lines = input.lines().map { it.toCharArray() }
     var code = 0
     var x = 1
@@ -25,14 +52,9 @@ fun part1(input: String, keypad: Array<Array<Int>>): Int {
 
 fun main() {
     val input = File("input.txt").readText().trim()
-    val keypad = arrayOf(
-        arrayOf(1, 2, 3),
-        arrayOf(4, 5, 6),
-        arrayOf(7, 8, 9)
-    )
     
-    val p1 = part1(input, keypad)
-    val p2 = part2(input, keypad)
+    val p1 = part1(input)
+    val p2 = part2(input)
 
     println("Part 1: $p1")   
     println("Part 2: $p2")
