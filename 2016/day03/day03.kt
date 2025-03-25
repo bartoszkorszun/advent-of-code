f5e69c0c9ed90803daca4707785ab9ddff4c24fc
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-03-25 11:02:53 +0100

    solved part 1

--- 2016/day03/day03.kt
+++ 2016/day03/day03.kt
@@ -1,18 +1,32 @@
 import java.io.File
 
-fun part2(input: String): Int {
+data class Triangle(val a: Int, val b: Int, val c: Int) {
+    fun isValid(): Boolean {
+        return a + b > c && a + c > b && b + c > a
+    }
+}
+
+fun parseInput(input: String): List<Triangle> {
+    return input.lines().map {
+        val sides = it.trim().split("\\s+".toRegex()).map { it.toInt() }
+        Triangle(sides[0], sides[1], sides[2])
+    }
+}
+
+fun part2(triangles: List<Triangle>): Int {
     return 0
 }   
 
-fun part1(input: String): Int {
-    return 0
+fun part1(triangles: List<Triangle>): Int {
+    return triangles.count { it.isValid() }
 }
 
 fun main() {
-    val input = File("test.txt").readText().trim()
+    val input = File("input.txt").readText().trim()
+    val triangles = parseInput(input)
     
-    val p1 = part1(input)
-    val p2 = part2(input)
+    val p1 = part1(triangles)
+    val p2 = part2(triangles)
 
     println("Part 1: $p1")   
     println("Part 2: $p2")
