61d05c8a30cf6a128f306818e78aad1acf488208
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-03-25 11:09:37 +0100

    solved part 2

--- 2016/day03/day03.kt
+++ 2016/day03/day03.kt
@@ -6,27 +6,39 @@ data class Triangle(val a: Int, val b: Int, val c: Int) {
     }
 }
 
-fun parseInput(input: String): List<Triangle> {
+fun parseInputPart1(input: String): List<Triangle> {
     return input.lines().map {
         val sides = it.trim().split("\\s+".toRegex()).map { it.toInt() }
         Triangle(sides[0], sides[1], sides[2])
     }
 }
 
-fun part2(triangles: List<Triangle>): Int {
-    return 0
+fun parseInputPart2(input: String): List<Triangle> {
+    val lines = input.lines().map { it.trim().split("\\s+".toRegex()).map { it.toInt() } }
+    val triangles = mutableListOf<Triangle>()
+    for (i in 0 until lines.size step 3) {
+        for (j in 0 until 3) {
+            triangles.add(Triangle(lines[i][j], lines[i + 1][j], lines[i + 2][j]))
+        }
+    }
+    return triangles
+}
+
+fun part2(input: String): Int {
+    val triangles = parseInputPart2(input)
+    return triangles.count { it.isValid() }
 }   
 
-fun part1(triangles: List<Triangle>): Int {
+fun part1(input: String): Int {
+    val triangles = parseInputPart1(input)
     return triangles.count { it.isValid() }
 }
 
 fun main() {
     val input = File("input.txt").readText().trim()
-    val triangles = parseInput(input)
     
-    val p1 = part1(triangles)
-    val p2 = part2(triangles)
+    val p1 = part1(input)
+    val p2 = part2(input)
 
     println("Part 1: $p1")   
     println("Part 2: $p2")
