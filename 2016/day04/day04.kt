ff5a3063dc24d63c8ccb3ebec2360313b9cc9fbd
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-03-26 12:25:47 +0100

    solved part 2

--- 2016/day04/day04.kt
+++ 2016/day04/day04.kt
@@ -17,8 +17,43 @@ fun parseInput(input: String): List<Room> {
 }
 
 fun part2(input: String): Int {
-    return 0
-}   
+    val rooms = parseInput(input)
+    val validRooms = rooms.filter { room ->
+        val charCount = room.name.groupingBy { it }.eachCount()
+        val sorted = charCount.entries.sortedWith(compareBy({ -it.value }, { it.key }))
+        val checksum = sorted.take(5).joinToString("") { it.key.toString() }
+        checksum == room.checksum
+    }
+    
+    val correctRooms = input.lines().map {
+        val parts = it.split("-")
+        val name = parts.dropLast(1).joinToString("-")
+        val sector = parts.last().split("[").first().toInt()
+        val checksum = parts.last().split("[").last().dropLast(1)
+        Room(name, sector, checksum)
+    }
+    
+    fun decrypt(name: String, shift: Int): String {
+        return name.map { char ->
+            if (char == '-') ' '
+            else {
+                val shifted = ((char.code - 'a'.code + shift) % 26) + 'a'.code
+                shifted.toChar()
+            }
+        }.joinToString("")
+    }
+    
+    for (room in correctRooms) {
+        if (validRooms.any { it.sector == room.sector }) { 
+            val decrypted = decrypt(room.name, room.sector)
+            if (decrypted.contains("northpole object storage")) {
+                return room.sector
+            }
+        }
+    }
+    
+    return -1
+}  
 
 fun part1(input: String): Int {
     val rooms = parseInput(input)
