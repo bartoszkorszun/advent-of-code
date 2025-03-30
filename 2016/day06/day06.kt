7a4e4d5d77a34ffe6a8a4649fb17f6fd62fa3482
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-03-30 20:43:21 +0200

    solved part 1

--- 2016/day06/day06.kt
+++ 2016/day06/day06.kt
@@ -1,18 +1,32 @@
 import java.io.File
 
-fun part2(input: String): Int {
-    return 0
+fun parseInput(input: String): Array<CharArray> {
+    return input.lines().map { it.toCharArray() }.toTypedArray()
+}
+
+fun part2(grid: Array<CharArray>): String {
+    return ""
 }   
 
-fun part1(input: String): Int {
-    return 0
+fun part1(grid: Array<CharArray>): String {
+    val message = StringBuilder()
+    for (i in grid[0].indices) {
+        val charCount = mutableMapOf<Char, Int>()
+        for (j in grid.indices) {
+            val char = grid[j][i]
+            charCount[char] = charCount.getOrDefault(char, 0) + 1
+        }
+        message.append(charCount.maxByOrNull { it.value }?.key ?: ' ')
+    }
+    return message.toString()
 }
 
 fun main() {
     val input = File("input.txt").readText().trim()
-    
-    val p1 = part1(input)
-    val p2 = part2(input)
+    val grid = parseInput(input)
+
+    val p1 = part1(grid)
+    val p2 = part2(grid)
 
     println("Part 1: $p1")   
     println("Part 2: $p2")
