d09fce7e95bbf9d2687020d9dcd455c58cf6b479
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-04-10 13:44:43 +0200

    solved part 1

--- 2016/day09/day09.kt
+++ 2016/day09/day09.kt
@@ -5,11 +5,26 @@ fun part2(input: String): Int {
 }   
 
 fun part1(input: String): Int {
-    return 0
+    var totalLength = 0
+    var i = 0
+    while (i < input.length) {
+        if (input[i] == '(') {
+            val end = input.indexOf(')', i)
+            val marker = input.substring(i + 1, end).split('x')
+            val length = marker[0].toInt()
+            val repeat = marker[1].toInt()
+            totalLength += length * repeat
+            i = end + 1 + length
+        } else {
+            totalLength++
+            i++
+        }
+    }
+    return totalLength
 }
 
 fun main() {
-    val input = File("test.txt").readText().trim()
+    val input = File("input.txt").readText().trim()
     
     val p1 = part1(input)
     val p2 = part2(input)
