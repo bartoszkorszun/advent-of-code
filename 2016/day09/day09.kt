4e45785501e2ce1c11cc98a0e162497369cc2889
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-04-10 13:53:45 +0200

    solved part 2

--- 2016/day09/day09.kt
+++ 2016/day09/day09.kt
@@ -1,7 +1,23 @@
 import java.io.File
 
-fun part2(input: String): Int {
-    return 0
+fun part2(input: String): Long {
+    var totalLength = 0L
+    var i = 0
+    while (i < input.length) {
+        if (input[i] == '(') {
+            val end = input.indexOf(')', i)
+            val marker = input.substring(i + 1, end).split('x')
+            val length = marker[0].toInt()
+            val repeat = marker[1].toLong()
+            val subString = input.substring(end + 1, end + 1 + length)
+            totalLength += part2(subString) * repeat
+            i = end + 1 + length
+        } else {
+            totalLength++
+            i++
+        }
+    }
+    return totalLength
 }   
 
 fun part1(input: String): Int {
